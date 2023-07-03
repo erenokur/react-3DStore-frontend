f24928370cc889625bd075071c067acf0e88b1fd
eren okur <32430029+erenokur@users.noreply.github.com>
2023-07-03 23:35:02 +0300

    3D object added.

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useRef } from "react";
+import "./App.css";
+import { Canvas, useFrame } from "@react-three/fiber";
+import Product from "./components/product";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ height: "100vh" }}>
+      <Canvas>
+        <ambientLight />
+        <pointLight position={[10, 10, 10]} />
+        <Product />
+      </Canvas>
     </div>
   );
 }
---
+++ src/components/product/index.tsx
@@ -0,0 +1,25 @@
+import { Canvas, useFrame } from "@react-three/fiber";
+import * as THREE from "three";
+import React, { useRef } from "react";
+
+const Product = () => {
+  const boxGeometry = new THREE.BoxGeometry(2, 2, 2);
+  const boxMaterial = new THREE.MeshLambertMaterial({ color: 0xffffff });
+  const boxMesh = useRef<THREE.Mesh>(null);
+  useFrame(() => {
+    if (boxMesh.current) {
+      boxMesh.current.rotation.x += 0.01;
+      boxMesh.current.rotation.y += 0.01;
+    }
+  });
+  return (
+    <mesh
+      geometry={boxGeometry}
+      material={boxMaterial}
+      position={[0, 0, 0]}
+      ref={boxMesh}
+    />
+  );
+};
+
+export default Product;
