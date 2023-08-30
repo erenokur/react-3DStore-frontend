8bc5e63915bbbf4bc4320c31c61131ab1883a940
eren okur <32430029+erenokur@users.noreply.github.com>
2023-08-30 12:38:33 +0300

    i18n error has been fixed

--- src/context/i18next/index.tsx
+++ src/context/i18next/index.tsx
@@ -1,22 +1,38 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
+import Backend from "i18next-xhr-backend";
 
 if (!i18n.isInitialized) {
-  i18n.use(initReactI18next).init({
-    lng: "en",
-    fallbackLng: "en",
-    debug: true,
-    detection: {
-      order: ["cookie", "navigator"],
-      caches: ["cookie"],
-    },
-    backend: {
-      loadPath: "/locales/{{lng}}/{{ns}}.json",
-    },
-    missingKeyHandler: function (lng, namespace, key, fallbackValue) {
-      console.warn(`Missing Translation Key ${key}`);
-    },
-  });
+  i18n
+    .use(Backend)
+    .use(initReactI18next)
+    .init(
+      {
+        lng: "en",
+        fallbackLng: "en",
+        debug: true,
+        detection: {
+          order: ["cookie", "navigator"],
+          caches: ["cookie"],
+        },
+        backend: {
+          loadPath: "/assets/locales/{{lng}}/{{ns}}.json",
+        },
+        missingKeyHandler: (
+          lngs: string,
+          ns: string,
+          key: string,
+          fallbackValue: string
+        ) => {
+          console.warn(`Missing Translation Key ${key}`);
+        },
+      },
+      (err) => {
+        if (err) {
+          console.error(err);
+        }
+      }
+    );
 }
 
 export default i18n;
