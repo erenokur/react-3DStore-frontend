c607ecc6c48f1e45d3d8ffab80ccb0cb6cb9eb56
eren okur <32430029+erenokur@users.noreply.github.com>
2023-08-30 16:48:05 +0300

    first model asset loading made

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,13 +1,32 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { useTranslation } from "react-i18next";
 import { useNavigate } from "react-router-dom";
+import { Canvas } from "@react-three/fiber";
+import { useLoader } from "@react-three/fiber";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { Stats, OrbitControls, Circle } from "@react-three/drei";
 
 const Home: React.FC = () => {
   const navigate = useNavigate();
   const { t } = useTranslation("translation", { useSuspense: false });
+  const gltf = useLoader(GLTFLoader, "../../assets/models/shipyard.glb");
 
   return (
     <div className="h-screen flex flex-col justify-center items-center bg-black-100">
+      <Canvas camera={{ position: [-0.5, 1, 2] }} shadows>
+        <directionalLight position={[3.3, 1.0, 4.4]} castShadow />
+        <primitive
+          object={gltf.scene}
+          position={[0, 1, 0]}
+          children-0-castShadow
+        />
+        <Circle args={[10]} rotation-x={-Math.PI / 2} receiveShadow>
+          <meshStandardMaterial />
+        </Circle>
+        <OrbitControls target={[0, 1, 0]} />
+        <axesHelper args={[5]} />
+        <Stats />
+      </Canvas>
       <h1 className="text-4xl font-bold mb-8">{t("greeting")}</h1>
       <button
         className="px-4 py-2 rounded-md bg-blue-500 text-white hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400"
